976a02569202efef8fb26a2510d5cbe14359f9c0
Skarstein-Hatlen <kjetil.skarstein-hatlen@hotmail.no>
2023-06-01 17:46:46 +0200

    Code cleaning, test and Readme

--- routes/utility.js
+++ routes/utility.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { Op } = require('sequelize');
+const { Op, Sequelize } = require('sequelize');
 const { Item, Category, Role, User } = require('../models');
 const crypto = require('crypto');
 const fetch = require('node-fetch');
 
+
+// Populates database and creates Admin user.
 router.post('/setup', async (req, res) => {
     try {
         const itemsCount = await Item.count();
@@ -16,7 +18,6 @@ router.post('/setup', async (req, res) => {
             throw new Error('Network response was not ok');
         }
         const itemsData = await response.json();
-
         console.log('Items Data: ', itemsData);
         for (const itemData of itemsData.data) {
             const { id, item_name, price, stock_quantity, sku, category } = itemData;
@@ -32,17 +33,14 @@ router.post('/setup', async (req, res) => {
                 categoryId: categoryObj.id,
             });
         }
-
         await Role.bulkCreate([
             { name: 'Admin' },
             { name: 'User' },
         ]);
-
         const adminRole = await Role.findOne({ where: { name: 'Admin' } });
         if (!adminRole) {
             return res.status(500).json({ message: 'Admin role not found.' });
         }
-
         const hashedPassword = 'P@ssword2023'; 
         await User.create({
             fullName: 'Admin User',
@@ -51,7 +49,6 @@ router.post('/setup', async (req, res) => {
             password: hashedPassword,
             roleId: adminRole.id,
         });
-
         return res.json({ message: 'Database populated successfully.' });
     } catch (error) {
         console.error('There has been a problem with your fetch operation: ', error);
@@ -59,18 +56,28 @@ router.post('/setup', async (req, res) => {
     }
 });
 
+
+//Search for items
 router.post('/search', async (req, res) => {
     try {
-        const { searchQuery } = req.body;
+        const { searchQuery, categoryName, sku } = req.body;
+        
+        let conditions = {};
+        if (searchQuery) {
+            conditions.name = Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('Item.name')), 'LIKE', `%${searchQuery.toLowerCase()}%`);
+        }
+        if (sku) {
+            conditions.sku = Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('Item.sku')), 'LIKE', `%${sku.toLowerCase()}%`);
+        }
+        if (categoryName) {
+            conditions['$Category.name$'] = Sequelize.where(Sequelize.fn('LOWER', Sequelize.col('Category.name')), 'LIKE', `%${categoryName.toLowerCase()}%`);
+        }
         const items = await Item.findAll({
-            where: {
-                [Op.or]: [
-                    { name: { [Op.iLike]: `%${searchQuery}%` } },
-                    { '$Category.name$': { [Op.iLike]: `%${searchQuery}%` } },
-                    { sku: { [Op.iLike]: `%${searchQuery}%` } },
-                ],
-            },
-            include: [Category],
+            where: conditions,
+            include: [{
+                model: Category,
+                required: true
+            }]
         });
         return res.json({ items });
     } catch (error) {
@@ -78,5 +85,6 @@ router.post('/search', async (req, res) => {
         return res.status(500).json({ message: 'An error occurred while searching for items.', error });
     }
 });
+
 
 module.exports = router;
